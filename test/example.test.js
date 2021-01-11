c92f7ff2a22ddc17633e3fd7d2c62e935fb9dd4d
harriet-the-spy <dpcairns@gmail.com>
2021-01-11 13:44:08 -0800

    add test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,31 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { countsAsAYes } from '../utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('it should return true when provided a word that starts with y', (expect) => {
     //Arrange
     // Set up your arguments and expectations
     const expected = true;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = false;
+    const actual = countsAsAYes('yeeellloo');
+
+    //Expect
+    // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+
+test('it should return false when provided a word that starts with anything but a y', (expect) => {
+    //Arrange
+    // Set up your arguments and expectations
+    const expected = false;
+    
+    //Act 
+    // Call the function you're testing and set the result to a const
+    const actual = countsAsAYes('naaah');
 
     //Expect
     // Make assertions about what is expected versus the actual result
